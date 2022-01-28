5392c79a52ef82cc1cf7ed4b7a8474b4b1b645ec
Zhongfeng <zzf0717@gmail.com>
2022-01-28 16:51:29 +0900

    modified MATLAB files

--- [1]MATLAB/DL Data Prepare/DL_DataGenerator.m
+++ [1]MATLAB/DL Data Prepare/DL_DataGenerator.m
@@ -1,9 +1,10 @@
 % Generate the training/test data for autoencoder denosing DOA application
 % DATE: 2022.01.07 by ZZF
-%
+%      2022.01.21 by ZZF
 % Input:
 %   s:              The signal source
 %   dataType:       Training data or test data
+%   operSys         The operating system used by the computer
 %   num_sample:     The number of samples
 %   SNR:            The signal noise power ratio
 %   M0:             Total number of antenna elements in the array
@@ -19,7 +20,7 @@
 
 % dada =load('denoised_data.mat','denoised_data')
 
-function [vec_all,vec_all_ori,Rxx_noisy,angle_doa] = DL_DataGenerator(s,dataType,num_sample,SNR,M0,nsnapshot,K,lambda)
+function [vec_all,vec_all_ori,Rxx_noisy,angle_doa] = DL_DataGenerator(s,dataType,operSys,num_sample,SNR,M0,nsnapshot,K,lambda)
 
 signalPower = 1;
 %% Random Angle Generation
@@ -38,6 +39,7 @@ vec_all_ori = zeros(M0^2, num_sample);
 
 Rxx_noisy = zeros(M0,M0,num_sample);
 for snr = 1 : length(SNR)
+    tStart = tic;
     for a = 1: num_sample
         h_Rxx = zeros(M0,M0);
         nvar = signalPower/(10^(SNR(snr)/10)); % noise power
@@ -69,55 +71,10 @@ for snr = 1 : length(SNR)
         [vec_all(:,a)]=DL_SCM_2_vec(h_Rxx,M0); % noisy SCM to vector
         [vec_all_ori(:,a)]=DL_SCM_2_vec(Rxx,M0); % noiseless SCM to vector
     end
+    % Save to the files.
+    DL_DataSave(dataType,operSys,SNR(snr),vec_all,vec_all_ori,angle_doa);
+    
+    tEnd = toc(tStart);
+    fprintf('[%s] %d dB SNR Data out, consumed %d s\n',dataType,SNR(snr),tEnd);
 end
-% Prepare for the DL model
-if isequal(dataType, 'train')
-    %% WINDOWS
-    % dataPath = 'C:\Users\HYPC300\OneDrive - 한양대학교\GitHub\Deeplearning-Autoencoder-DOA\data\2022_01_21\train\train_data';
-    % originPath = 'C:\Users\HYPC300\OneDrive - 한양대학교\GitHub\Deeplearning-Autoencoder-DOA\data\2022_01_21\train\origin_data';
-    % labelPath = 'C:\Users\HYPC300\OneDrive - 한양대학교\GitHub\Deeplearning-Autoencoder-DOA\data\2022_01_21\train\train_label';
-    %% MAC
-    dataPath = '/Users/button/Deeplearning-Autoencoder-DOA/data/train/train_data';
-    originPath = '/Users/button/Deeplearning-Autoencoder-DOA/data/train/origin_data';
-    labelPath = '/Users/button/Deeplearning-Autoencoder-DOA/data/train/train_label';
-    %% Generate test data: Shuffle
-    train_data = vec_all.'; % make the data conform to numsample by (number antenna element^2(noisy))
-    tarData = 'train_data';
-    save(dataPath, tarData)
-    
-    origin_data = vec_all_ori.'; % make the data conform to numsample by (number antenna element^2(noisy))
-    tarData = 'origin_data';
-    save(originPath, tarData)
-    
-    %% Generate label (DOA angle)
-    train_label = angle_doa;
-    tarData = 'train_label';
-    save(labelPath, tarData)
-else
-    %% WINDOWS: Save Path
-    % dataPath = 'C:\Users\HYPC300\OneDrive - 한양대학교\GitHub\Deeplearning-Autoencoder-DOA\data\2022_01_21\test\train_data';
-    % originPath = 'C:\Users\HYPC300\OneDrive - 한양대학교\GitHub\Deeplearning-Autoencoder-DOA\data\2022_01_21\test\origin_data';
-    % labelPath = 'C:\Users\HYPC300\OneDrive - 한양대학교\GitHub\Deeplearning-Autoencoder-DOA\data\2022_01_21\test\test_label';
-    
-    %% MAC
-    dataPath = '/Users/button/Deeplearning-Autoencoder-DOA/data/test/test_data';
-    originPath = '/Users/button/Deeplearning-Autoencoder-DOA/data/test/origin_data';
-    labelPath = '/Users/button/Deeplearning-Autoencoder-DOA/data/test/test_label';
-    %% Generate test data: Shuffle
-    test_data = vec_all.'; % make the data conform to numsample by (number antenna element^2(noisy))
-    % Generate random number
-    % rng(23)
-    % ind_ran = randperm(num_sample);
-    tarData = 'test_data';
-    save(dataPath, tarData)
-    
-    %% Generate test data: Shuffle
-    test_origin_data = vec_all_ori.'; % make the data conform to numsample by (number antenna element^2(noisy))
-    tarData = 'test_origin_data';
-    save(originPath, tarData)
-    
-    %% Generate label (DOA angle)
-    test_label = angle_doa;
-    tarData = 'test_label';
-    save(labelPath, tarData)
 end
